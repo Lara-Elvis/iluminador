432232628122308b9605009b2b8733c53bfe8da2
Elvis Lara <laraelvisinar@gmail.com>
2024-03-22 21:18:48 -0600

    agrega cambios y diseño y carpeta imagens

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const container = document.getElementById("container");
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
-const SQUARES = 500;
+const SQUARES = 400;
+const LIGHT_UP_DURATION = 100;
+const FADE_OUT_DELAY = 500;
 
 for(let i = 0; i < SQUARES; i++){
     const square = document.createElement("div");
@@ -15,14 +17,21 @@ for(let i = 0; i < SQUARES; i++){
 function setColor(element){
     const color = getRandomColor();
     element.style.background = color;
-    element.style.boxShadow = `0 0 2px ${color} 0 0 10px ${color}`;
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+
+    setTimeout(() => {
+        element.style.background = color;
+        element.style.boxShadow = '0 0 2px #000';
+    }, LIGHT_UP_DURATION);
 }
 
 function remeveColor(element){
-    element.style.background = '#1d1d1d';
-    element.style.boxShadow = '0 0 2px #000'
+    setTimeout(() => {
+        element.style.background = '#1d1d1d';
+        element.style.boxShadow = '0 0 2px #090';
+    }, FADE_OUT_DELAY);
 }
 
 function getRandomColor(){
-    return colors[Math.floor(Math.random() = colors.length)];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
